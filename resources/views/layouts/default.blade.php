15541f1733ad562fcc8ce18250d834cd65686867
Paul Cross <me@paulcross.cz>
2023-12-08 18:40:47 +0100

    wip

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,10 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel HTMX Todo</title>
 
-    <script src="https://cdn.tailwindcss.com"></script>
-    {{-- <script src="https://unpkg.com/htmx.org@1.9.9"></script> --}}
+    @vite('resources/css/app.css')
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
