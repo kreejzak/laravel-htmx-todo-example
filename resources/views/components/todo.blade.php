51f610709743495a0700d28c4a2b7ebed98d8018
Paul Cross <me@paulcross.cz>
2023-12-08 21:42:48 +0100

    cleanup

--- resources/views/components/todo.blade.php
+++ resources/views/components/todo.blade.php
@@ -1,11 +1,14 @@
 <li>
-    <label for="todo-{{ $todo->id }}"
-        class="flex justify-between items-center py-2 px-3 w-full rounded shadow  toggle animate duration-200 bg-gray-900 {{ $todo->completed ? 'opacity-20' : '' }}">
+    <label for="todo-{{ $todo->id }}" @class([
+        'toggle animate  flex w-full items-center justify-between rounded bg-gray-900 px-3 py-2 shadow duration-200',
+        'opacity-20' => $todo->completed,
+    ])>
         <div class="flex items-center space-x-2">
             <form hx-target="closest li" hx-swap="outerHTML">
                 @csrf
-                <input hx-put="todos/{{ $todo->id }}" id="todo-{{ $todo->id }}" name="checked" type="checkbox"
-                    {{ $todo->completed ? 'checked' : '' }} />
+                <input hx-put="todos/{{ $todo->id }}"
+                    id="todo-{{ $todo->id }}" name="checked" type="checkbox"
+                    @checked($todo->completed) />
 
             </form>
             <div>
@@ -13,7 +16,8 @@
             </div>
         </div>
         <div class="flex items-center space-x-2">
-            <form hx-confirm="Are you sure?" hx-delete="todos/{{ $todo->id }}" hx-target="closest li"
+            <form hx-confirm="Are you sure?"
+                hx-delete="todos/{{ $todo->id }}" hx-target="closest li"
                 hx-swap="delete">
                 @csrf
                 <button class="text-sm text-red-500" type="submit">
